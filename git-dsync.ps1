d1a097a1ff4eff2d793f6a83f812f3d0641fd456
Joshua Wiens <jrwiens0707@gmail.com>
2018-02-13 14:10:03 -0600

    finished dsync

--- git-dsync.ps1
+++ git-dsync.ps1
@@ -1,6 +1,7 @@
-# git checkout master
+git checkout master
+git pull origin master
 $branches = git branch | Select-String -NotMatch "\*"
-foreach $branch in $branches
+foreach ($branch in $branches)
 {
-    
+    git pull origin $branch
 }
